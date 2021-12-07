1791ea5b0f247bd1d25af06b10e2a25fa1b684e6
Hrishikesh Bharati <hrishi1096@gmail.com>
2021-12-07 16:19:54 +0000

    product_search.py: browserstack remote driver integrated
    XXX: script failing at the moment because amazon.com is not working
    Try again after sometime and ensure that the capabilities are set right

--- product_search.py
+++ product_search.py
@@ -12,16 +12,48 @@ from selenium.common.exceptions import NoSuchFrameException
 from selenium.common.exceptions import NoSuchElementException
 
 from bs4 import BeautifulSoup
+from threading import Thread
 
 
 
 AMAZON_URL = "https://www.amazon.com"
 PRODUCT = "iphone X"
 WEBDRIVER_IMPLICIT_WAIT_TIME = 10
+USERNAME = os.environ['BROWSERSTACK_USERNAME']
+ACCESSKEY = os.environ['BROWSERSTACK_ACCESS_KEY']
 
 
-def setup():
-	driver = webdriver.Safari()
+capabilities = [{
+      'os_version': '11',
+      'os': 'Windows',
+      'browser': 'chrome',
+      'browser_version': '96.0',
+      'name': 'Chrome Test',
+      'build': 'Amazon_product_search_build'
+      },
+      {
+      'os_version': '10',
+      'os': 'Windows',
+      'browser': 'firefox',
+      'browser_version': '94.0',
+      'name': 'Firefox test',
+      'build': 'Amazon_product_search_build'
+      },
+      {
+      'os_version': 'Monterey',
+      'os': 'OS X',
+      'browser': 'safari',
+      'browser_version': '15.0',
+      'name': 'Safari',
+      'build': 'Amazon_product_search_build'
+}]
+
+
+
+def setup(desired_cap):
+	driver = webdriver.Remote(
+		command_executor='https://' + USERNAME + ':' + ACCESSKEY + '@hub-cloud.browserstack.com/wd/hub',
+		desired_capabilities=desired_cap)
 	driver.implicitly_wait(WEBDRIVER_IMPLICIT_WAIT_TIME)
 	driver.maximize_window()
 	return driver
@@ -66,47 +98,66 @@ def get_element(driver, css_selector):
 	except NoSuchElementException:
 		print("{}: NoSuchElementException\n".format(css_selector))
 
-	teardown(driver)
 	return None
 
 
 def get_search_result_url(search_str):
-	template = "https://www.amazon.com/s?k={}&ref=nb_sb_noss_2"
+	template = AMAZON_URL + "/s?k={}&ref=nb_sb_noss_2"
 	search_str = search_str.replace(' ', '+')
 	return template.format(search_str)
 
 
-def amazon_search(product):
+def amazon_search(driver, product):
 	ios_filter_css    = '#p_n_feature_twenty_browse-bin\\/17881878011 > span > a > div > label > i'
 	sort_dropdown_css = '#a-autoid-0-announce'
 	high_to_low_css   = '#s-result-sort-select_2'
-
-	# Setup driver
-	driver = setup()
 
 	search_result_url = get_search_result_url(product)
 
    	# Go directly to the search results page
 	driver.get(search_result_url)
 
-    # Apply operating system filter: iOS
+	# Apply operating system filter: iOS
 	ios_filter = get_element(driver, ios_filter_css)
+	if (ios_filter is None):
+		return 0
 	ios_filter.click()
 
 	# Sort the search results from High to Low price
 	sort_dropdown = get_element(driver, sort_dropdown_css)
+	if (sort_dropdown is None):
+		return 0
 	sort_dropdown.click()
 
 	high_to_low = get_element(driver, high_to_low_css)
+	if (high_to_low is None):
+		return 0
 	high_to_low.click()
 
 	logger(driver)
+
+	return 1
+
+
+def run_product_search(desired_cap):
+	# Setup
+	driver = setup(desired_cap)
+
+	if (amazon_search(driver, PRODUCT)):
+		driver.execute_script(
+			'browserstack_executor: {"action": "setSessionStatus", "arguments": \
+			{"status":"passed", "reason": "Amazon product search successful!"}}')
+	else:
+		driver.execute_script(
+			'browserstack_executor: {"action": "setSessionStatus", "arguments": \
+			{"status":"failed", "reason": "Amazon product search unsuccessful"}}')
 
 	# Cleanup
 	teardown(driver)
 
 
 if __name__ == "__main__":
-	amazon_search(PRODUCT)
+    for cap in capabilities:
+        Thread(target=run_product_search, args=(cap,)).start()
 
 
