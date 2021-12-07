ff0e4c06818e8508e67bcf8278378ade1e5d6160
Hrishikesh Bharati <hrishi1096@gmail.com>
2021-12-07 13:15:09 +0000

    product_search.py: logger implemented, works on all Chrome, Firefox and Safari now
    TODO: hook it up with the browserstack remote driver now for parallel tests

--- product_search.py
+++ product_search.py
@@ -7,12 +7,12 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchFrameException
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchFrameException
+from selenium.common.exceptions import NoSuchElementException
 
+from bs4 import BeautifulSoup
 
-import time
 
 
 AMAZON_URL = "https://www.amazon.com"
@@ -21,38 +21,62 @@ WEBDRIVER_IMPLICIT_WAIT_TIME = 10
 
 
 def setup():
-	driver = webdriver.Firefox()
+	driver = webdriver.Safari()
 	driver.implicitly_wait(WEBDRIVER_IMPLICIT_WAIT_TIME)
 	driver.maximize_window()
 	return driver
+
 
 def teardown(driver):
 	driver.quit()
 
 
-def logger():
-	pass
+def logger(driver):
+	index = 0
+	soup = BeautifulSoup(driver.page_source, 'html.parser')
+	all_results = soup.find_all('div', {'data-component-type': 's-search-result'})
+
+	for result in all_results:
+		index += 1
+		atag = result.h2.a
+		name = atag.text.strip()
+		link = AMAZON_URL + atag.get('href')
+		price = result.find('span', 'a-price')
+		price_text = "Not available"
+		if price is not None:
+			price_text = price.find('span', 'a-offscreen').text
+
+		print(
+			  "\n--------{}--------"
+			  "\nProduct name: {}"
+			  "\nPrice: {} "
+			  "\nLink to product details:\n{}".format(index, name, price_text, link)
+			  )
 
 
 def get_element(driver, css_selector):
-	delay = 5
+	delay = 10
 	try:
 	    elem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
 	    return elem
 	except TimeoutException:
-	    print("Loading is taking too long!")
+	    print("{}: Loading is taking too long!\n".format(css_selector))
 	except NoSuchFrameException:
-		print("NoSuchFrameException")
+		print("{}: NoSuchFrameException\n".format(css_selector))
 	except NoSuchElementException:
-		print("NoSuchElementException")
+		print("{}: NoSuchElementException\n".format(css_selector))
 
 	teardown(driver)
 	return None
 
 
+def get_search_result_url(search_str):
+	template = "https://www.amazon.com/s?k={}&ref=nb_sb_noss_2"
+	search_str = search_str.replace(' ', '+')
+	return template.format(search_str)
+
+
 def amazon_search(product):
-	textbox_css       = '#twotabsearchtextbox'
-	search_button_css = '#nav-search-submit-button'
 	ios_filter_css    = '#p_n_feature_twenty_browse-bin\\/17881878011 > span > a > div > label > i'
 	sort_dropdown_css = '#a-autoid-0-announce'
 	high_to_low_css   = '#s-result-sort-select_2'
@@ -60,49 +84,23 @@ def amazon_search(product):
 	# Setup driver
 	driver = setup()
 
-   	# Go to the desired url
-	driver.get(AMAZON_URL)
+	search_result_url = get_search_result_url(product)
 
-	# Find the textbox, clear it, enter the product name
-	textbox = get_element(driver, textbox_css)
-	if textbox is None:
-		print("Couldn't find textbox")
-		return
-
-	textbox.clear()
-	textbox.send_keys(product)
-
-	# Find and click the search button
-	search_button = get_element(driver, search_button_css)
-	if search_button is None:
-		print("Couldn't find search_button")
-		return
-	search_button.click()
+   	# Go directly to the search results page
+	driver.get(search_result_url)
 
     # Apply operating system filter: iOS
 	ios_filter = get_element(driver, ios_filter_css)
-	if ios_filter is None:
-		print("Couldn't find ios_filter")
-		return
 	ios_filter.click()
 
 	# Sort the search results from High to Low price
 	sort_dropdown = get_element(driver, sort_dropdown_css)
-	if sort_dropdown is None:
-		print("Couldn't find sort_dropdown")
-		return
-
 	sort_dropdown.click()
 
 	high_to_low = get_element(driver, high_to_low_css)
-	if high_to_low is None:
-		print("Couldn't find high_to_low entry")
-		return
-
 	high_to_low.click()
 
-	# TODO: call the logger here
-	print("The logger needs to be called now")
+	logger(driver)
 
 	# Cleanup
 	teardown(driver)
