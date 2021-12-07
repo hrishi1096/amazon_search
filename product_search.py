c34439af9d0ea51aaf6d7803d16bd097edae22d9
Hrishikesh Bharati <hrishi1096@gmail.com>
2021-12-07 23:59:12 +0000

    product_search.py: logger and amazon_search function refactored
    Additionally, this commit has the following changes:
    * logger doesn't use BeautifulSoup anymore, it's all selenium now
    * using amazon.co.uk now because there were some issues with amazon.com
    * get_search_url function removed, pure automation right from amazon home page now
    * css selectors modified to reflect it's functionality clearly

--- product_search.py
+++ product_search.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 # To run from the terminal
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -11,14 +12,14 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchFrameException
 from selenium.common.exceptions import NoSuchElementException
 
-from bs4 import BeautifulSoup
 from threading import Thread
 
 
-
-AMAZON_URL = "https://www.amazon.com"
+AMAZON_URL = "https://www.amazon.co.uk"
+CURRENCY = "Pounds"
 PRODUCT = "iphone X"
 WEBDRIVER_IMPLICIT_WAIT_TIME = 10
+DELAY = 5
 USERNAME = os.environ['BROWSERSTACK_USERNAME']
 ACCESSKEY = os.environ['BROWSERSTACK_ACCESS_KEY']
 
@@ -29,7 +30,7 @@ capabilities = [{
       'browser': 'chrome',
       'browser_version': '96.0',
       'name': 'Chrome Test',
-      'build': 'Amazon_product_search_build'
+      'build': 'Amazon_product_search_build_1'
       },
       {
       'os_version': '10',
@@ -37,15 +38,15 @@ capabilities = [{
       'browser': 'firefox',
       'browser_version': '94.0',
       'name': 'Firefox test',
-      'build': 'Amazon_product_search_build'
+      'build': 'Amazon_product_search_build_1'
       },
       {
       'os_version': 'Monterey',
       'os': 'OS X',
       'browser': 'safari',
       'browser_version': '15.0',
-      'name': 'Safari',
-      'build': 'Amazon_product_search_build'
+      'name': 'Safari Test',
+      'build': 'Amazon_product_search_build_1'
 }]
 
 
@@ -64,32 +65,34 @@ def teardown(driver):
 
 
 def logger(driver):
-	index = 0
-	soup = BeautifulSoup(driver.page_source, 'html.parser')
-	all_results = soup.find_all('div', {'data-component-type': 's-search-result'})
+    index = 0
+    all_results = driver.find_elements(By.CSS_SELECTOR, "div[data-component-type='s-search-result']")
 
-	for result in all_results:
-		index += 1
-		atag = result.h2.a
-		name = atag.text.strip()
-		link = AMAZON_URL + atag.get('href')
-		price = result.find('span', 'a-price')
-		price_text = "Not available"
-		if price is not None:
-			price_text = price.find('span', 'a-offscreen').text
+    for result in all_results:
+        index += 1
+        name = result.find_element(By.CSS_SELECTOR, 'span.a-size-medium.a-color-base.a-text-normal')
+        name = name.text
 
-		print(
-			  "\n--------{}--------"
-			  "\nProduct name: {}"
-			  "\nPrice: {} "
-			  "\nLink to product details:\n{}".format(index, name, price_text, link)
-			  )
+        try:
+            price = result.find_element(By.CSS_SELECTOR, 'span.a-price-whole')
+            price = price.text
+        except NoSuchElementException:
+            price = "Currently not available"
+
+        link = result.find_element(By.CSS_SELECTOR, 'a.a-link-normal.a-text-normal')
+        link = link.get_attribute('href')
+
+        print(
+              "\n--------{}--------"
+              "\nProduct name: {}"
+              "\nPrice(in {}): {}"
+              "\nLink to product details:\n{}".format(index, name, CURRENCY, price, link)
+              )
 
 
 def get_element(driver, css_selector):
-	delay = 10
 	try:
-	    elem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+	    elem = WebDriverWait(driver, DELAY).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
 	    return elem
 	except TimeoutException:
 	    print("{}: Loading is taking too long!\n".format(css_selector))
@@ -101,42 +104,64 @@ def get_element(driver, css_selector):
 	return None
 
 
-def get_search_result_url(search_str):
-	template = AMAZON_URL + "/s?k={}&ref=nb_sb_noss_2"
-	search_str = search_str.replace(' ', '+')
-	return template.format(search_str)
+def accept_cookies(driver, css_selector):
+    try:
+        cookie_accept = WebDriverWait(driver, DELAY).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+        cookie_accept.click()
+    except NoSuchElementException:
+        return
 
 
 def amazon_search(driver, product):
-	ios_filter_css    = '#p_n_feature_twenty_browse-bin\\/17881878011 > span > a > div > label > i'
-	sort_dropdown_css = '#a-autoid-0-announce'
-	high_to_low_css   = '#s-result-sort-select_2'
+    cookie_accept_css = "input[name='accept']"
+    searchbox_css     = "input[id='twotabsearchtextbox']"
+    submit_btn_css    = "input[id='nav-search-submit-button']"
+    ios_filter_css    = "li[aria-label='iOS']"
+    sort_dropdown_css = 'span[data-action="a-dropdown-button"]'
+    high_to_low_css   = '#s-result-sort-select_2'
 
-	search_result_url = get_search_result_url(product)
+    # Go to desired url
+    driver.get(AMAZON_URL)
 
-   	# Go directly to the search results page
-	driver.get(search_result_url)
+    # To handle the cookies banner
+    accept_cookies(driver, cookie_accept_css)
 
-	# Apply operating system filter: iOS
-	ios_filter = get_element(driver, ios_filter_css)
-	if (ios_filter is None):
-		return 0
-	ios_filter.click()
+    # Search the product
+    searchbox = get_element(driver, searchbox_css)
+    if searchbox is None:
+    	return 0
+    searchbox.clear()
+    searchbox.send_keys(product)
 
-	# Sort the search results from High to Low price
-	sort_dropdown = get_element(driver, sort_dropdown_css)
-	if (sort_dropdown is None):
-		return 0
-	sort_dropdown.click()
+    # Click submit
+    submit_btn = get_element(driver, submit_btn_css)
+    if searchbox is None:
+    	return 0
+    submit_btn.click()
 
-	high_to_low = get_element(driver, high_to_low_css)
-	if (high_to_low is None):
-		return 0
-	high_to_low.click()
+    # This is only for the sake of Safari
+    time.sleep(3)
 
-	logger(driver)
+    # Apply operating system filter: iOS
+    ios_filter = get_element(driver, ios_filter_css)
+    if searchbox is None:
+    	return 0
+    ios_filter.click()
 
-	return 1
+    # Sort the search results from High to Low price
+    sort_dropdown = get_element(driver, sort_dropdown_css)
+    if searchbox is None:
+    	return 0
+    sort_dropdown.click()
+
+    high_to_low = get_element(driver, high_to_low_css)
+    if searchbox is None:
+    	return 0
+    high_to_low.click()
+
+    logger(driver)
+
+    return 1
 
 
 def run_product_search(desired_cap):
